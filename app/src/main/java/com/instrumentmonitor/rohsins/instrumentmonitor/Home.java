0b40991a696d40fa422c4f2d15d555ca8cdecce3
Rohit Singh <rohit.singh@rts.com.np>
2015-11-09 17:15:05 +0545

    initial commit

---
+++ app/src/main/java/com/instrumentmonitor/rohsins/instrumentmonitor/Home.java
@@ -0,0 +1,39 @@
+package com.instrumentmonitor.rohsins.instrumentmonitor;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class Home extends Sockets {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            Intent intentSettings = new Intent(this, Settings.class);
+            startActivity(intentSettings);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void bkPrecisionMultimeter(View view) {
+        Intent intentBKPrecisionMultimeter = new Intent(this, BKPrecisionMeter.class);
+        startActivity(intentBKPrecisionMultimeter);
+    }
+}
